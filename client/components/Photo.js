d8f4beb02a2aa02f082d3f71206f91664a1ccfc2
harishankards <harishankards@gmail.com>
2017-06-18 23:10:38 +0530

    Link comments and add button styles

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -24,6 +24,12 @@ const Photo = React.createClass( {
           <p>{post.caption}</p>
           <div className="control-buttons">
             <button className="likes">&hearts;{post.likes}</button>
+            <Link className="button" to={`/view/${post.code}`}>
+              <span className="comment-count">
+                <span className="speech-bubble"></span>
+                {comments[post.code] ? comments[post.code].length : 0}
+              </span>
+            </Link>
           </div>
         </figcaption>
       </figure>
