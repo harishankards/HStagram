9bec9cd863eb40b81c6aaea7099851855edff7dc
harishankards <harishankards@gmail.com>
2017-06-18 23:03:30 +0530

    Display photos in the data, Add caption and like button

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 const Photo = React.createClass( {
   render(){
@@ -8,9 +9,23 @@ const Photo = React.createClass( {
       <figure className="grid-figure">
         <div className="grid-photo-wrap">
           <Link to={`/view/${post.code}`}>
-            {post.caption}
+            <img src={post.display_src} alt={post.caption} className="grid-photo" />
           </Link>
+
+          <CSSTransitionGroup transitionName="like"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={500}>
+            <span key={post.likes} className="likes-heart">
+              {post.likes}
+            </span>
+          </CSSTransitionGroup>
         </div>
+        <figcaption>
+          <p>{post.caption}</p>
+          <div className="control-buttons">
+            <button className="likes">&hearts;{post.likes}</button>
+          </div>
+        </figcaption>
       </figure>
     );
   }
